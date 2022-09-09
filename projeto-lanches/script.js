8a5c6a72ab2848d0752956732fbe5a33fa3fffa4
Vitzk <93144306+Vitzk@users.noreply.github.com>
2022-09-09 14:59:40 -0300

    ion-icon
    Esqueci de incluir o aparecimento do icone, segue atualização.

--- projeto-lanches/script.js
+++ projeto-lanches/script.js
@@ -20,9 +20,11 @@ function selecionarHamburguer(hamburguerClicado) {
     const hamburguer = document.querySelector(".selecionado1");
     if (hamburguer !== null ) {
         hamburguer.classList.remove("selecionado1"); 
+        hamburguer.querySelector("ion-icon").classList.add("oculto");
     } 
     
     hamburguerClicado.classList.add("selecionado1"); 
+    hamburguerClicado.querySelector("ion-icon").classList.remove("oculto");
 
     hamburguerNome = hamburguerClicado.querySelector("h3").innerText;
     hamburguerValor = hamburguerClicado.querySelector("h4").innerText;
@@ -36,8 +38,10 @@ function selecionarAcompanhamento(acompanhamentoClicado) {
     const acompanhamento = document.querySelector(".selecionado2");
     if (acompanhamento !== null) {
         acompanhamento.classList.remove("selecionado2");
+        acompanhamento.querySelector("ion-icon").classList.add("oculto");
     }
     acompanhamentoClicado.classList.add("selecionado2");
+    acompanhamentoClicado.querySelector("ion-icon").classList.remove("oculto");
 
     acompanhamentoNome = acompanhamentoClicado.querySelector("h3").innerText;
     acompanhamentoValor = acompanhamentoClicado.querySelector("h4").innerText;
@@ -50,9 +54,11 @@ function selecionarBebida(bebidaClicado) {
     const bebida = document.querySelector(".selecionado3");
     if (bebida !== null) {
         bebida.classList.remove("selecionado3");
+        bebida.querySelector("ion-icon").classList.add("oculto");
     }
     
     bebidaClicado.classList.add("selecionado3");
+    bebidaClicado.querySelector("ion-icon").classList.remove("oculto");
 
     bebidaNome = bebidaClicado.querySelector("h3").innerText;
     bebidaValor = bebidaClicado.querySelector("h4").innerText;
@@ -69,6 +75,11 @@ function fecharPedido() {
         total = (hamburguerValor + acompanhamentoValor + bebidaValor);
     }
 
+}
+
+function confirmarPedido() {
+    document.querySelector(".confirmacao").classList.remove("oculto");
+    
 }
 
 function enviarPedido() {
